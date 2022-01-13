43b0db94118bcfaa04bbf99f5e519382bc8dafbd
Amali Jayawardhana <amali.j@hsenidmobile.com>
2022-01-13 07:26:40 +0530

    Get request and response

--- src/main/resources/features/login.feature
+++ src/main/resources/features/login.feature
@@ -1,16 +1,23 @@
 Feature: login
-  This feature for login page '
+  This feature for login page
 
   @UAT @All @LoginPage
-  Scenario: User send the message and verify the response
+ Scenario Outline: User send the message and verify the response
     Given user go to Amazon login page
-    When user type username as username
-    When user type password for password
+    When user type "<username>" and "<password>"
+#    When user type username
+#    When user type username as "<username>"
+#    When user type password as "<password>"
+#    When user type password
     And   click on login button
-#    When  user type password for password
-#    And   click on login button
     Then  user should logged in successfully
-    Then  user select the action as test in the dropdown
-    Then  user input the message
-    Then  user input the message2
+#    Then  user select the action as test in the dropdown
+    Then  user input the "<message1>" and "<message2>" and "<message3>"
     And   user verify response message
+
+ Examples:
+ |username|password|message1|message2|message3|
+#|testalicealexa@gmail.com|test@SKILLS|hi      |good morning|Thank you|
+|testalicealexa@gmail.com|test@SKILLS|Alexa, open my business      |Alexa, start my business|close my business|
+
+
--- src/test/java/steps/UserLoginStepDefinition.java
+++ src/test/java/steps/UserLoginStepDefinition.java
@@ -9,74 +9,77 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.io.IOException;
+import java.util.ListIterator;
 
 public class UserLoginStepDefinition extends Base {
-        private String actualResponseMessage1 = "";
-        private String actualResponseMessage = "";
-//        private final By RESPONSE_MESSAGE = By.xpath("//div[@id='astro-tabs-1-panel-0']/div[2]/div[2]/p");
-        private final By RESPONSE_MESSAGE = By.cssSelector(".askt-dialog__message.askt-dialog__message--active-response");
-        private final By RESPONSE_MESSAGE1 = By.cssSelector(".askt-dialog__bubble.askt-dialog__bubble--response");
+    private String actualResponseMessage1 = "";
+    private String actualResponseMessage = "";
+    //        private final By RESPONSE_MESSAGE = By.xpath("//div[@id='astro-tabs-1-panel-0']/div[2]/div[2]/p");
+    private final By RESPONSE_MESSAGE = By.cssSelector(".askt-dialog__message.askt-dialog__message--active-response");
+    private final By RESPONSE_MESSAGE1 = By.cssSelector(".askt-dialog__bubble.askt-dialog__bubble--response");
 
 
 //        private final By ErrorDescriptionForInvalid = By.xpath("//span[contains(text(),'Invalid username or password.')]");
 
-        @Before
-        public void setup() {
-            String exePath = "src/main/resources/drivers/local/chromedriver";
-            //String exePath = "src/main/resources/drivers/server/chromedriver";
+    @Before
+    public void setup() {
+        String exePath = "src/main/resources/drivers/local/chromedriver";
+        //String exePath = "src/main/resources/drivers/server/chromedriver";
 
-            System.setProperty("webdriver.chrome.driver", exePath);
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--no-sandbox"); //Bypass OS security model
-            options.addArguments("--start-maximized");
-            options.addArguments("--disable-dev-shm-usage");
+        System.setProperty("webdriver.chrome.driver", exePath);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox"); //Bypass OS security model
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-dev-shm-usage");
 //            options.addArguments("--headless");
-            options.addArguments("ignore-certificate-errors");
-            options.addArguments("--ignore-ssl-errors=yes");
-            dr = new ChromeDriver(options);
-        }
+        options.addArguments("ignore-certificate-errors");
+        options.addArguments("--ignore-ssl-errors=yes");
+        dr = new ChromeDriver(options);
+    }
 
-        @After
-        public void cleanUp() {
-            dr.quit();
-        }
+    @After
+    public void cleanUp() {
+        dr.quit();
+    }
 
-        @Given("user go to Amazon login page")
-        public void userGoToTalitoLoginPage() throws IOException {
-            dr.get(getProperty("BaseURL"));
-            dr.manage().window().maximize();
-        }
+    @Given("user go to Amazon login page")
+    public void userGoToTalitoLoginPage() throws IOException {
+        dr.get(getProperty("BaseURL"));
+        dr.manage().window().maximize();
+    }
 
 
-        @When("user type username as username")
-        public void userTypeUsernameAsDaAdmin() throws IOException {
-            //dr.findElement(By.xpath("//*[@id='username']")).sendKeys(getProperty("daAdminUsername"));
-            dr.findElement(By.id("ap_email")).sendKeys(getProperty("username"));
-        }
+    @When("^user type username as \"(.*)\"$")
+    public void userTypeUsernameAsDaAdmin(String username) throws IOException {
+        //dr.findElement(By.xpath("//*[@id='username']")).sendKeys(getProperty("daAdminUsername"));
+        dr.findElement(By.id("ap_email")).sendKeys(username);
+    }
 
-        @When("user type password for password")
-        public void userTypePasswordForDaAdmin() throws IOException {
+    @When("^user type password as \"(.*)\"$")
+    public void userTypePasswordForDaAdmin(String password) throws IOException {
 //            dr.findElement(By.xpath("//*[@id='password']")).sendKeys(getProperty("daAdminPassword"));
-            dr.findElement(By.id("ap_password")).sendKeys(getProperty("password"));
-        }
+        dr.findElement(By.id("ap_password")).sendKeys(password);
+    }
 
-        @When("^click on login button$")
-        public void clickLoginButton() throws Throwable{
-            Thread.sleep(1000);
+
+    @When("^click on login button$")
+    public void clickLoginButton() throws Throwable {
+        Thread.sleep(1000);
 //            dr.findElement(By.id("signInSubmit")).click();
-            JavascriptExecutor jse = (JavascriptExecutor)dr;
-            jse.executeScript("arguments[0].click()", dr.findElement(By.id("signInSubmit")));
+        JavascriptExecutor jse = (JavascriptExecutor) dr;
+        jse.executeScript("arguments[0].click()", dr.findElement(By.id("signInSubmit")));
 
-        }
+    }
 
-        @Then("^user should logged in successfully$")
-        public void verifySuccessful() throws Throwable{
-            Thread.sleep(5000);
-            boolean icon = dr.findElement(By.xpath("//*[@id=\"dpc-header-logo\"]/div/div[2]/span")).isDisplayed();
+    @Then("^user should logged in successfully$")
+    public void verifySuccessful() throws Throwable {
+        Thread.sleep(5000);
+        boolean icon = dr.findElement(By.xpath("//*[@id=\"dpc-header-logo\"]/div/div[2]/span")).isDisplayed();
 //            boolean question = dr.findElement(By.xpath("//*[@id=\"root\"]/span/div/div/div/div/ul/div/div/h2")).isDisplayed();
 //            boolean welcome = dr.findElement(By.xpath("//*[@id=\"root\"]/span/div/div/div/div/ul/div/div/h4")).isDisplayed();
 
@@ -85,53 +88,73 @@ public class UserLoginStepDefinition extends Base {
 //            boolean eng = dr.findElement(By.xpath("//*[@id=\"landing-page-space-1\"]")).isDisplayed();
 //            boolean survey = dr.findElement(By.xpath("//*[@id=\"landing-page-space-2\"]")).isDisplayed();
 
-            Assert.assertArrayEquals(new boolean[]{true}, new boolean[]{
-                    icon});
-        }
-        @When("user select the action as test in the dropdown")
-        public void loginUser() throws InterruptedException {
-            Thread.sleep(30000);
+        Assert.assertArrayEquals(new boolean[]{true}, new boolean[]{
+                icon});
+    }
+
+    @When("user select the action as test in the dropdown")
+    public void loginUser() throws InterruptedException {
+        Thread.sleep(30000);
 //            dr.findElement(By.id("react-select-2--value")).click();
-            JavascriptExecutor jse = (JavascriptExecutor)dr;
-            jse.executeScript("arguments[0].click()", dr.findElement(By.id("react-select-2--value")));
+        JavascriptExecutor jse = (JavascriptExecutor) dr;
+        jse.executeScript("arguments[0].click()", dr.findElement(By.id("react-select-2--value")));
 
-            jse.executeScript("arguments[0].click()", dr.findElement(By.id("react-select-3--value")));
-        }
+        jse.executeScript("arguments[0].click()", dr.findElement(By.id("react-select-3--value")));
+    }
 
-        @When("user input the message")
-        public void userTypeInputMessage() throws Throwable {
-            dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys("Hi");
-            dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys(Keys.ENTER);
-            Thread.sleep(10000);
+    @When("^user type \"(.*)\" and \"(.*)\"$")
+    public void userTypeUsernamePassword(String username, String password) throws IOException {
+        //dr.findElement(By.xpath("//*[@id='username']")).sendKeys(getProperty("daAdminUsername"));
+        dr.findElement(By.id("ap_email")).sendKeys(username);
+        dr.findElement(By.id("ap_password")).sendKeys(password);
 
-        }
-        @When("user input the message2")
-        public void userTypeInputMessage2() throws Throwable {
-            dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys("Good morning");
-            dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys(Keys.ENTER);
-            Thread.sleep(10000);
+    }
 
-        }
+    @Then("^user input the \"(.*)\" and \"(.*)\" and \"(.*)\"$")
+    public void userTypeInputMessage(String message1, String message2, String message3) throws Throwable {
+        Thread.sleep(10000);
+        dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys(message1);
+        dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys(Keys.ENTER);
+        Thread.sleep(10000);
+        dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys(message2);
+        dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys(Keys.ENTER);
+        Thread.sleep(10000);
+        dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys(message3);
+        dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div/div[2]/input")).sendKeys(Keys.ENTER);
+        Thread.sleep(10000);
+    }
 
-        @When("user verify response message")
-        public void userVerifyResponseMessage() throws Throwable {
-            Thread.sleep(10000);
+    @When("user verify response message")
+    public void userVerifyResponseMessage() throws Throwable {
+        Thread.sleep(10000);
+        boolean request = dr.findElement(By.cssSelector(".askt-dialog__bubble--request")).isDisplayed();
+        boolean response = dr.findElement(By.cssSelector(".askt-dialog__message--response")).isDisplayed();
+
 //            boolean responseMessage = dr.findElement(By.xpath("//div[@id='astro-tabs-1-panel-0']/div[2]/div[2]/p")).isDisplayed();
 //            boolean responseMessage = dr.findElement(By.cssSelector(".askt-dialog__message.askt-dialog__message--request")).isDisplayed();
-            boolean responseMessage = dr.findElement(By.cssSelector(".askt-dialog__message.askt-dialog__message--active-response")).isDisplayed();
-            Assert.assertArrayEquals(new boolean[]{true}, new boolean[]{
-                    responseMessage});
-            actualResponseMessage = dr.findElement(RESPONSE_MESSAGE).getText();
-            actualResponseMessage1 = dr.findElement(RESPONSE_MESSAGE1).getText();
-            
-            System.out.println("actual Message    "+actualResponseMessage);
-            System.out.println("actual Message1    "+actualResponseMessage1);
-            Thread.sleep(10000);
+//        boolean responseMessage = dr.findElement(By.cssSelector(".askt-dialog__message.askt-dialog__message--active-response")).isDisplayed();
+        Assert.assertArrayEquals(new boolean[]{true, true}, new boolean[]{
+                request, response});
+        ListIterator<WebElement> requestMessageElements = dr.findElements(By.cssSelector(".askt-dialog__message--request")).listIterator();
+        ListIterator<WebElement> responseMessageElements = dr.findElements(By.cssSelector(".askt-dialog__message--response")).listIterator();
+
+        for (ListIterator<WebElement> it = requestMessageElements; it.hasNext(); ) {
+            WebElement requestMessageElement = it.next();
+            String requestMessage = requestMessageElement.getText();
+            System.out.println("request Message " + ": " + requestMessage);
+        }
+        for (ListIterator<WebElement> it = responseMessageElements; it.hasNext(); ) {
+            WebElement responseMessageElement = it.next();
+            String responseMessage = responseMessageElement.getText();
+            System.out.println("request Message " + ": " + responseMessage);
 
         }
 
+        dr.findElements(By.cssSelector(".askt-dialog__message--request"));
+
+        Thread.sleep(10000);
+
+    }
 
 
-
-
-            }
+}
